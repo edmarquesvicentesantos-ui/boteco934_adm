7192e273cc66327abaa15b5a5e2472106dfe97e3
edmarquesvicentesantos-ui <edmarquesvicentesantos@gmail.com>
2026-04-11 15:32:17 -0300

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,30 @@ const db = firebase.database();
 let carrinho = [];
 
 // Escutar Produtos do Firebase em Tempo Real
-db.ref('produtos').on('value', (snapshot) => {
+db.ref('produtos').on('value', (snapshot) => <script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAxjhzLPeqBqJ18S8m7lagxuvF9LX7OJks",
+    authDomain: "boteco934-afc3f.firebaseapp.com",
+    databaseURL: "https://boteco934-afc3f-default-rtdb.firebaseio.com",
+    projectId: "boteco934-afc3f",
+    storageBucket: "boteco934-afc3f.firebasestorage.app",
+    messagingSenderId: "182023728304",
+    appId: "1:182023728304:web:040a13bb6f61c9fff35f75",
+    measurementId: "G-HWT2RYRMRQ"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
     const produtos = snapshot.val();
     renderizarProdutos(produtos);
     verificarEstoqueCritico(produtos);
